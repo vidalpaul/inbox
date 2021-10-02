656b39b82d2f1881635b8c589c9ef3c39e4c2483
Paulo Vidal <u1d4lp@gmail.com>
2021-10-02 14:50:46 -0300

    assert contract deployment

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,17 +3,28 @@ const ganache = require('ganache-cli');
 //web3 is capitalized because it is a constructor function
 const Web3 = require('web3');
 
+const { interface, bytecode } = require('../compile');
+
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
    // Get a list of all accounts
    accounts = await web3.eth.getAccounts();
-   console.log(accounts);
+
    // Use one of those accounts to deploy the contract
+   inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({
+         data: bytecode,
+         arguments: ['Hello wworld'],
+      })
+      .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-   it('deploys a contract', () => {});
+   it('deploys a contract', () => {
+      assert.ok(inbox.options.address);
+   });
 });
